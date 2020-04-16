b0b36e585fdd388af9a6c534d34c89b99510cfea
RebeccaRoach <rebeccaroach12@gmail.com>
2020-04-16 16:29:36 -0700

    Implemented generateSquareComponents in Board.js to render board squares.

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,13 +5,25 @@ import PropTypes from 'prop-types';
 
 
 const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
+  // squares is an array of arrays [[1,2,3], [4,5,6], [7,8,9]]
+  // modifying squares so that each index contains a Square Component
+  
+  for (const row in squares) {
+    for (const col in squares[row]) {
+      squares[row][col] = <Square 
+                              id={squares[row][col].id}
+                              value={squares[row][col].value}
+                              // onClickCallback={onClickCallback}
+                          />
+    }
+  }
 
+  return squares;
 }
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
+  // console.log(squareList);
   return <div className="grid" >
     {squareList}
   </div>
