f40ceaf2afe7a4ed22fc973a1aa412e2a8839063
SuelyBarreto <suely@barreto.us>
2020-04-18 14:51:34 -0700

    checkforwinner part 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Board from './components/Board';
 
 const PLAYER_1 = 'X';
 const PLAYER_2 = 'O';
+const NOPLAYER = '' ;
 
 const generateSquares = () => {
   const initialSquares = [];
@@ -15,7 +16,7 @@ const generateSquares = () => {
     for (let col = 0; col < 3; col += 1) {
       initialSquares[row].push({
         id: currentId,
-        value: "",
+        value: NOPLAYER,
       });
       currentId += 1;
     }
@@ -31,6 +32,8 @@ const App = () => {
   // State
   const [squares, setSquares] = useState(generateSquares());
   const [player, setPlayer]   = useState(PLAYER_1);
+  const [winner, setWinner]   = useState(NOPLAYER);
+
 
   // Callback function for square click
   const onClickCallback = (id) => {
@@ -39,7 +42,7 @@ const App = () => {
     const row = Math.floor(id / 3);
     const col = id % 3;
     
-    if (squares[row][col].value === "") {
+    if (squares[row][col].value === NOPLAYER && winner === NOPLAYER) {
       // if square available, mark it for the player
       squares[row][col].value = player;
       setSquares(squares);
@@ -50,21 +53,39 @@ const App = () => {
 
   }
 
-  const checkForWinner = () => {
-    // Complete in Wave 3
+  const checkForThree = (row1, col1, row2, col2, row3, col3) => {
+    if (squares[row1][col1].value === squares[row2][col2].value &&
+        squares[row1][col1].value === squares[row3][col3].value &&
+        squares[row1][col1].value !== NOPLAYER &&
+        winner === NOPLAYER
+    ) {
+        setWinner(squares[row1][col1].value);
+    }
+  }
 
+  const checkForWinner = () => {
+    for (let i = 0; i < 3; i++) {
+      checkForThree(i, 0, i, 1, i, 2);
+      checkForThree(0, i, 1, i, 2, i);
+    }
+    checkForThree(0, 0, 1, 1, 2, 2);
+    checkForThree(2, 0, 1, 1, 0, 2);
   }
 
   const resetGame = () => {
     // Complete in Wave 4
   }
 
+
+  checkForWinner();
+
+  const message = winner === NOPLAYER ? `Player is ${player}` : `Winner is ${winner}`;
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
-        <h2>Player is {player} </h2>
+        <h2>{message}</h2>
         <button>Reset Game</button>
       </header>
       <main>
