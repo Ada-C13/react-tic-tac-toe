22c3e6344f5b5335482785c93c5e2f5ef6e0c205
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-18 13:52:49 -0700

    Wave 3 - returns winning lines and indexes of Xs and Os

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Board from './components/Board';
 const PLAYER_1 = 'X';
 const PLAYER_2 = 'O';
 
+const WINNING_LINES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const generateSquares = () => {
   const squares = [];
 
@@ -47,6 +58,9 @@ const App = () => {
 
           setSquares(newBoard);
           switchPlayer(currentPlayer);
+          console.log(returnXIndexes(newBoard));
+          console.log(returnOIndexes(newBoard));
+          console.log(checkForWinner(newBoard));
           return;
         };
       };
@@ -57,11 +71,62 @@ const App = () => {
     player === PLAYER_1 ? setCurrentPlayer(PLAYER_2) : setCurrentPlayer(PLAYER_1);
   }
 
-  const checkForWinner = () => {
-    // Complete in Wave 3
+  function returnXIndexes(board) {
 
+    let checkWinner = [...squares].flat()
+    let indexXCollection = []
+
+    checkWinner.filter(function(square) {
+      if(square.value === 'X'){
+        indexXCollection.push(square.id)
+      };
+    });
+    return indexXCollection;
   }
 
+  function returnOIndexes(board) {
+
+    let checkWinner = [...squares].flat()
+    let indexOCollection = []
+
+    checkWinner.filter(function(square) {
+      if(square.value === 'O'){
+        indexOCollection.push(square.id)
+      };
+    });
+    return indexOCollection;
+  }
+
+  // For wave 3, you will add the game logic to detect if a player has won or if there is a tie (all squares filled and with no winner). To do this you will complete the checkForWinner method and display the winner in the header section. The game should also cease responding to clicks on the board if the game has a winner.
+  const checkForWinner = (newBoard) => {
+    // Complete in Wave 3
+    let winner = null
+
+    WINNING_LINES.forEach(combination => {
+      // if(returnOIndexes(newBoard).includes(combination)){
+      //   winner = PLAYER_2
+      // }
+      // else if(returnXIndexes(newBoard).includes(combination)){
+      //   winner = PLAYER_1
+      // }
+      // if(returnOIndexes(newBoard).includes(combination)){
+      //  return true;}
+      // else if(returnXIndexes(newBoard).includes(combination)){
+      //   return true;}
+      if(combination.every(e => returnOIndexes(newBoard).includes(e))){
+        winner = PLAYER_2;
+        console.log('THIS TIME IT WAS '+ PLAYER_2 )
+      }
+      else if(combination.every(e => returnXIndexes(newBoard).includes(e))){
+        winner = PLAYER_1;
+        console.log('THIS TIME IT WAS '+ PLAYER_1 )
+      };
+      // console.log(returnOIndexes(newBoard).includes(combination))
+    });
+    // console.log(winner)
+  }
+
+  
   //Event handler
   const resetGame = () => {
     // Complete in Wave 4
@@ -73,6 +138,7 @@ const App = () => {
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
         <h2>The winner is ... ${currentPlayer} </h2>
+        {/* Event listener */}
         <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
