2cf736e63eeb0d4c6596b30502cdd24f62d25589
Rosalyn <syrosalynyu@gmail.com>
2020-04-18 16:33:50 -0700

    test

--- src/App.js
+++ src/App.js
@@ -35,20 +35,18 @@ const App = () => {
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
   const onClickCallback = ( (squareId) => {
-    // mark the square either X or O
-    // who's turn => get from the App.js (start from X, which is PLAYER 1)
-    // get the id => from the Square.js
     let copiedSquares = [...squares];
     for(let row of copiedSquares) {
       for(let square of row) {
        if (square.id === squareId && square.value === '') {
           square.value = isXTurn ? PLAYER_1 : PLAYER_2;
           setXTurn(!isXTurn);
+          
         } 
       }
     }
     setSquares(copiedSquares);
-    // checkForWinner();
+    checkForWinner(copiedSquares);
   }) 
 
 
@@ -56,6 +54,30 @@ const App = () => {
     // Complete in Wave 3
     // if rows, cols, diagonal has no same pattern => no winner
     // else the square.value ( PLAYER1 or PLAYER2 ) is the winner
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    
+    for(let pattern of winningPatterns) {
+      console.log("testing print" + newSquares[pattern[0]].value);
+      console.log(newSquares);
+      // if (newSquares[pattern[0]]["value"] &&
+      //     newSquares[pattern[0]["value"]] === newSquares[pattern[2]["value"]] &&
+      //     newSquares[pattern[1]["value"]] === newSquares[pattern[2]["value"]]
+      //   ) {
+      //     console.log('got a winner')
+      //     return newSquares[pattern[0]];
+      //   }
+      }  
+    console.log('no winner');
+    return null;
 
     }  
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -25,3 +25,28 @@ Square.propTypes = {
 };
 
 export default Square
+
+
+// const winningPatterns = [
+    //   [0, 1, 2],
+    //   [3, 4, 5],
+    //   [6, 7, 8],
+    //   [0, 3, 6],
+    //   [1, 4, 7],
+    //   [2, 5, 8],
+    //   [0, 4, 8],
+    //   [2, 4, 6]
+    // ];
+    
+    // for(let pattern of winningPatterns) {
+    //   console.log(newSquares[pattern[1]["value"]]);
+    //   if (newSquares[pattern[0]["value"]] &&
+    //       newSquares[pattern[0]["value"]] === newSquares[pattern[2]["value"]] &&
+    //       newSquares[pattern[1]["value"]] === newSquares[pattern[2]["value"]]
+    //     ) {
+    //       console.log('got a winner')
+    //       return newSquares[pattern[0]];
+    //     }
+    //   }  
+    // console.log('no winner');
+    // return null;
