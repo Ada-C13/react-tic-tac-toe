5b46df7fc228b77baf4f5b8131f99bd3741d3853
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-19 09:55:03 -0700

    Final clean ups

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,6 @@ import './Board.css';
 import Square from './Square';
 import PropTypes from 'prop-types';
 
-//ASSIGNMENT REQ: Update the Board component to render the grid of squares. You will need to complete the generateSquareComponents function in the Board component.
 const generateSquareComponents = (squares, onClickCallback) => {
   // Complete this for Wave 1
   let squareComponents
--- src/components/Square.js
+++ src/components/Square.js
@@ -4,14 +4,8 @@ import PropTypes from 'prop-types';
 import './Square.css'
 
 const Square = (props) => {
-  // For Wave 1 enable this 
-  //  Component to alert a parent 
-  //  component when it's clicked on.
-  return (
-    <button 
-    className="square" 
-    key={props.id} 
-    onClick={() => {props.onClickCallback(props.id)}} >
+
+  return (<button className="square" key={props.id} onClick={() => {props.onClickCallback(props.id)}} >
       {props.value}
     </button>)
 }
