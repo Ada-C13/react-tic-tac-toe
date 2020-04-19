c7e91adfc1b650146aef9491a6aefac53ef33711
Stephanie <stephaniejmars@gmail.com>
2020-04-19 16:44:29 -0700

    winner displaying and tie. nn to make game stop when winner detected

--- src/App.js
+++ src/App.js
@@ -39,27 +39,30 @@ const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
   const [player, setPlayer] = useState(PLAYER_1)
+  const [winner, setWinner] = useState(null)
+  const [filledSquareCount, setFilledSquareCount] = useState(1)
 
   const onClickCallBack = (id) => {
-    // console.log(id)
-      const newSquares = [];
-      squares.forEach(row => {
-        const newRow = []
-        row.forEach(square => {
-          if (square.id === id) {
-            square.value = player
-            setPlayer(player === PLAYER_1 ? PLAYER_2 : PLAYER_1)
-            //could eventually set it so it can only be changed once
-          }
-          newRow.push(square)
-        })
-        newSquares.push(newRow)
+
+    setFilledSquareCount(filledSquareCount + 1);
+    // set it to stop when winner detected 
+    const newSquares = [];
+    squares.forEach(row => {
+      const newRow = []
+      row.forEach(square => {
+        if (square.id === id) {
+          square.value = player
+          setPlayer(player === PLAYER_1 ? PLAYER_2 : PLAYER_1);
+          //could eventually set it so it can only be changed once
+          checkForWinner();
+          console.log(filledSquareCount)
+        }
+        newRow.push(square)
       })
-      setSquares(newSquares);
-  }
-  //we define it in app bc the funtion will nn access to the state of the gam e
-  //we need to call the function in squre bc thats where the button is 
-  //responsible for knowing when something happens to it 
+      newSquares.push(newRow)
+    })
+    setSquares(newSquares);
+  };
 
 
   const checkForWinner = () => {
@@ -70,26 +73,25 @@ const App = () => {
     });
 
     WINNING_COMBOS.forEach(row=> {
-        // console.log(row)
-        if(squareValues[row[0]] === squareValues[row[1]] && squareValues[row[1]] === squareValues[row[2]]){
-          // console.log(`Winner is ${squareValues[row[0]].value}`)
-        }
+        if(squareValues[row[0]] !== "" && squareValues[row[0]] === squareValues[row[1]] && squareValues[row[1]] === squareValues[row[2]]){
+          return setWinner(squareValues[row[0]]);
+        } else if (filledSquareCount === 9){
+          return setWinner('tie');
+        };
       });
-  
-    
-    // console.log(squareValues)
-
-  }
+      return null;
+  };
 
   const resetGame = () => {
     setSquares(generateSquares)
-  }
+    setFilledSquareCount(1)
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... {checkForWinner()}</h2>
+        <h2>The winner is ... {winner}</h2>
         <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
