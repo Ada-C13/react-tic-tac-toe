439ee5af59099db6cbaf834e6bc11e7e65616825
Vera <lizeth.veraro@outlook.com>
2020-04-19 01:43:28 -0700

    refactoring checkForWinner, WINNINGOPTIONS as a constant

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import Board from './components/Board';
 const PLAYER_1 = 'X';
 const PLAYER_2 = 'O';
 
+
+const WINNINGOPTIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 const generateSquares = () => {
   const squares = [];
 
@@ -29,77 +41,65 @@ const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
   const [itsPlayer1Turn, setPlayer1Turn] = useState(true);
+  // const [winner, setWinner] = useState(undefined);
  
   // Wave 2
   // You will need to create a method to change the square 
   // When it is clicked on.
   // Then pass it into the squares as a callback
 
-
   const updateValue = (squareClickedId) => {
+    
     const squareList = [];
 
     squares.forEach((row, i) => {
-      // console.log(row);
-      // console.log(i);
       squareList.push([]);
-      // squareList.push(row)
-      // console.log(squareList);
       row.forEach((square) => {
-        // console.log(square)
-        if (square.id === squareClickedId.id && square.value === "" ) {
-
-          // squareClickedId.value = PLAYER_1
-
+        if (square.id === squareClickedId.id && square.value === "") {
           squareClickedId.value = itsPlayer1Turn ? PLAYER_1 : PLAYER_2;
           setPlayer1Turn(!itsPlayer1Turn);
-
           squareList[i].push(squareClickedId)
-          // squareList.push(squareClickedId)
         } else {
-
           squareList[i].push({...square})
         }
       });
     });
-    console.log(squareList);
     setSquares(squareList);
   }
 
-  const checkForWinner = (setSquares) => {
-    // Complete in Wave 3
-
-    const winningOptions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
+  // It returns an array with the current clicked values. 
+  const extractValuesFromSquares = ((setSquares) => {
     const values = [];
-
+    
     setSquares.forEach((row) => {
       row.forEach((square) => {
         console.log(row)
         values.push(square.value)
       });
     });
+    return values;
+  });
 
-    for (let i in winningOptions) {
-      const [a, b, c] = winningOptions[i];
+  const checkForWinner = (setSquares) => {
+    // Complete in Wave 3
+    // Invoking the function to get a flat array with the values. 
+    const values = extractValuesFromSquares(setSquares);
+
+    for (let i in WINNINGOPTIONS) {
+      // Destructuring (Example winningOptions first Iteration[i] i=0 
+      // a = 0, b = 1 c = 2)
+      const [a, b, c] = WINNINGOPTIONS[i];
+      // Return the value of the winner
       if (values[a] && values[a] === values[b] && values[a] === values[c]) {
           return values[a];
         }
       }
+      // If there is a tie return null.
       return null;
   }
 
   const winner = checkForWinner(squares);
-
+  // setPlaying(checkForWinner(squares));
   console.log(winner);
 
   const resetGame = () => {
