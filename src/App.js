3f5eaa535e50502e47fc3b1df5e5e91c124526d0
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-04-19 02:50:17 -0700

    Completed Wave 2  mark X or O on the square based on currentPlayer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-
 import Board from './components/Board';
 
 const PLAYER_1 = 'X';
@@ -28,16 +27,35 @@ const generateSquares = () => {
 const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
+  //For the hook:
+    //initial vaule of the state object is a 2D array of square objects
+    // squares is the current value of the state
+    // setSquare is a function to change the state
 
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
 
+  // This updateSquare function is executed when the square is clicked via onClickCallback
+  // How do we want to update the square? : mark X or O on it the clicked sqaure
+
+  const [currentPlayer, setPlayer] = useState(PLAYER_1) // the first player is always PLAYER_1
+  const updateSquare = (id) =>{
+    const newSquareList = squares.slice();
+      for (let i = 0; i < newSquareList.length; i++){
+        for (let j = 0 ; j < newSquareList.length; j++){
+          if (newSquareList[i][j].id === id && newSquareList[i][j].value === ""){
+            newSquareList[i][j].value = currentPlayer? PLAYER_1 : PLAYER_2
+          }
+          setPlayer(!currentPlayer)
+        }
+      }  
+      setSquares(newSquareList)
+    }
 
   const checkForWinner = () => {
     // Complete in Wave 3
-
   }
 
   const resetGame = () => {
@@ -52,7 +70,7 @@ const App = () => {
         <button>Reset Game</button>
       </header>
       <main>
-        <Board squares={squares} />
+        <Board squares={squares} onClickCallback = {updateSquare} />
       </main>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,40 +3,48 @@ import './Board.css';
 import Square from './Square';
 import PropTypes from 'prop-types';
 
+// Complete this for Wave 1
+// We want to display all the squares but each Square will be responsible for displaying each square
+// By looking at the App.js, App component is passing information to <Board/> component
+// The information  <Board/> gets from App component is squares={squares} a props name is square with the value {squares}
+// Note: squares is 2D array of object 
+// So, with this given information, the Board component is responsible to renders all the squres 
+// Our objective here: how we get a list of squares from the given 2D array of squares(objects)?
+//Note: Square component will take 2 props from Board component (id and value)
 const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
-  const allSquares = squares.flat()
-  const list = allSquares.map((square) =>{
+// Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+  const arrayOfSquareObj= squares.flat() // now we have an array of square objects but we want an array of Square component
+  const arrayOfSquareComponents = arrayOfSquareObj.map((square) => { // square is an object with props of id and value
     return(
-      <Square 
+      <Square
         id = {square.id}
         value = {square.value}
         key = {square.id}
+        onClickCallback = {onClickCallback} //Wave 2 passing the onClickCallback go Square component
       />
-    );
-  }); 
-  return list;
-};
-
-const Board = ({ squares, onClickCallback }) => {
-  const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
- 
-  return <div className="grid" >
-    {squareList}
-  </div>
+    )
+  });
+  return arrayOfSquareComponents; //generateSquareComponents returns an arrayOfSquareComponents
 }
 
+const Board = (props) => { //deconstructed given ({squares, onClickCallback}) to props  
+const squareList = generateSquareComponents(props.squares, props.onClickCallback); // to get the value of squares, use props.squares
+// console.log(squareList);
+return <div className="grid" >
+  {squareList}
+</div>
+}
+// By looking at the propTypes, we also have onClickCallback that is props that gets from the parent App Component
 Board.propTypes = {
-  squares: PropTypes.arrayOf(
-    PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        value: PropTypes.string.isRequired
-      })
-    )
-  ),
-  onClickCallback: PropTypes.func.isRequired,
+squares: PropTypes.arrayOf(
+  PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      value: PropTypes.string.isRequired
+    })
+  )
+),
+onClickCallback: PropTypes.func.isRequired,
 };
-
+    
 export default Board;
