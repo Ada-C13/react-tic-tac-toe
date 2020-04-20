0dce57f18a5df0cae6b203d9e7134103af719653
Yieni <yaynee8d@gmail.com>
2020-04-20 00:41:51 -0700

    complete wave 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-
 import Board from './components/Board';
 
 const PLAYER_1 = 'X';
@@ -30,26 +29,28 @@ const App = () => {
   const [squares, setSquares] = useState(generateSquares());
   const [currentPlayer, setCurrentPlayer] = useState(true)
   const [winner, setCurrentWinner] = useState(null);
-  const [count, setCurrenCount] = useState(0);
-
+  const [count, setCurrentCount] = useState(0)
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
 
   const onClickCallback = (id) => {
+    if (winner === "PLAYER_1" || winner === "PLAYER_2") return
     const newSquare = [...squares] 
     let i = 0
     while(i < squares.length){
-      let j = 0
-      while (j < squares.length){
-        if (squares[i][j].id === id && squares[i][j].value === "" ){
-          squares[i][j].value = currentPlayer? PLAYER_1 : PLAYER_2;
+      let x = 0
+      while (x < squares.length){
+        if (squares[i][x].id === id && squares[i][x].value === "" ){
+          squares[i][x].value = currentPlayer? PLAYER_1 : PLAYER_2;
           setCurrentPlayer(!currentPlayer)
-        }     
-        j++
+          setCurrentWinner(checkForWinner())
+          setCurrentCount(count + 1)
+        };    
+        x += 1
       }
-      i++
+      i += 1
     }
    setSquares(newSquare);
   }
@@ -63,8 +64,72 @@ const App = () => {
     if(flatSquare[0].value !== '' && 
     (flatSquare[0].value === flatSquare[4].value && 
       flatSquare[0].value === flatSquare[8].value) ) {
-        setCurrentWinner(flatSquare[0].value)
+      if(flatSquare[0].value === 'X') {
+        return "PLAYER_1"
+      } else {
+        return "PLAYER_2"
+      };
+    } else if (flatSquare[2].value !== '' && 
+    (flatSquare[2].value === flatSquare[4].value && 
+      flatSquare[2].value === flatSquare[6].value) ) {
+        if(flatSquare[2].value === 'X') {
+          return "PLAYER_1"
+        } else {
+          return "PLAYER_2"
+        };
+
+      } else {
+      let x = 0
+      let i = 0
+      while(x < flatSquare.length && i < flatSquare.length){
+        if (flatSquare[x].value !== "" &&
+          flatSquare[x].value === flatSquare[x + 3].value &&
+          flatSquare[x].value === flatSquare[x + 6].value){
+          if(flatSquare[x].value === 'X') {
+            return "PLAYER_1"
+          } else {
+            return "PLAYER_2"
+          }
+        }; 
+
+        if (flatSquare[i].value !== "" &&
+          flatSquare[i].value === flatSquare[i + 1].value &&
+          flatSquare[i].value === flatSquare[i + 2].value){
+          if(flatSquare[i].value === 'X') {
+            return "PLAYER_1"
+          } else {
+            return "PLAYER_2"
+          };
+        };
+        x += 1
+        i += 3
       }
+
+      if (count === 8 ) {
+        return "no one. It's a tie!"
+      }; 
+    };
+    
+    
+    // else {
+    //   while(i < flatSquare.length) {
+    //     if (flatSquare[i].value !== "" &&
+    //       flatSquare[i].value === flatSquare[i + 1].value &&
+    //       flatSquare[i].value === flatSquare[i + 2].value){
+    //       if(flatSquare[i].value === 'X') {
+    //         return "PLAYER_1"
+    //       } else {
+    //         return "PLAYER_2"
+    //       }
+    //     }
+    //     i += 3
+    //   }
+    // } 
+
+
+    
+    
+    
 
   }
 
@@ -76,7 +141,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is {checkForWinner} -- Fill in for wave 3 </h2>
+        <h2>The winner is {winner} </h2>
         <button>Reset Game</button>
       </header>
       <main>
