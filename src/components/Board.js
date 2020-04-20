214a6cd66ed6e8bcd063897cac3fc6c9c1362232
Jocelyn Wang <wangjoc@gmail.com>
2020-04-19 21:55:18 -0700

    completed initial rendering of squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Board.css';
 import Square from './Square';
 import PropTypes from 'prop-types';
 
+// const onClickCallback = () => {
+//   console.log("test")
+// }
 
 const generateSquareComponents = (squares, onClickCallback) => {
   // Complete this for Wave 1
+  // {allSquares}
+  
+  console.log(onClickCallback)
 
+  const allSquares = squares.map (row =>
+    row.map (square =>
+      <Square key={square.id} id={square.id} value='X'/>
+    )
+  )
+
+  return (
+    
+    <div>
+        {allSquares}
+    </div>
+  )
 }
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
+  // console.log(squareList);
   return <div className="grid" >
     {squareList}
   </div>
