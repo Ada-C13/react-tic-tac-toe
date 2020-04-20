a676db08a5546bcebceb3b6fb4e720eb92cbcfed
Yieni <yaynee8d@gmail.com>
2020-04-19 22:33:07 -0700

    starting wave 3

--- src/App.js
+++ src/App.js
@@ -29,13 +29,16 @@ const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
   const [currentPlayer, setCurrentPlayer] = useState(true)
+  const [winner, setCurrentWinner] = useState(null);
+  const [count, setCurrenCount] = useState(0);
+
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
 
   const onClickCallback = (id) => {
-    let newSquare = [...squares] 
+    const newSquare = [...squares] 
     let i = 0
     while(i < squares.length){
       let j = 0
@@ -54,6 +57,15 @@ const App = () => {
   const checkForWinner = () => {
     // Complete in Wave 3
 
+    const newSquare = [...squares]
+    const flatSquare = newSquare.flat()
+
+    if(flatSquare[0].value !== '' && 
+    (flatSquare[0].value === flatSquare[4].value && 
+      flatSquare[0].value === flatSquare[8].value) ) {
+        setCurrentWinner(flatSquare[0].value)
+      }
+
   }
 
   const resetGame = () => {
@@ -64,7 +76,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <h2>The winner is {checkForWinner} -- Fill in for wave 3 </h2>
         <button>Reset Game</button>
       </header>
       <main>
