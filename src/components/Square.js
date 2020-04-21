8fabe9571f67b3d984b7ea8a9b7b0d1e2e2c3fdd
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-20 23:07:01 -0700

    added wave 1 components

--- src/components/Square.js
+++ src/components/Square.js
@@ -8,11 +8,14 @@ const Square = (props) => {
   //  Component to alert a parent 
   //  component when it's clicked on.
 
-  return <button
-    className="square"
+  return (
+    <button
+      className="square"
+      onClick={() => props.onClickCallback(props.id)}
   >
     {props.value}
   </button>
+  );
 }
 
 Square.propTypes = {
