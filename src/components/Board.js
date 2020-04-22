8e7c079285775e35c011ff74443913b14506a728
thenora <thenora@gmail.com>
2020-04-22 00:46:20 -0700

    cleaned up comments on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,33 +3,20 @@ import './Board.css';
 import Square from './Square';
 import PropTypes from 'prop-types';
 
-// css groups
-  // div.grid
-
-
 // This component will take a callback function, onClickCallback and a list of 2D array of JavaScript objects with ids, and values and will render Square components each with ids, values and the callback function passed in as props.
 
 const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
   // squares is array of arrays
 
-  // can I refactor to flatten first and then map?
+  // TODO can I refactor to flatten first and then map?
 
-  // loop through array of squares
-
-  return squares.flat().map( square =>
+  return squares.flat().map( square => // loop through array of squares
       <Square   // square component
-        // do I need a key? ID makes a good key
         id={square.id} // id set to props
         value={square.value} // value set to props
         onClickCallback={onClickCallback}  // onClickCallback
       />
     );
- 
-  // Update the Board component to render the grid of squares. You will need to complete the generateSquareComponents function in the Board component.
-
-  // App should pass to Board a 2D array of JavaScript objects and Board should use that to render an array of Square components.
-
 }
 
 const Board = ({ squares, onClickCallback }) => {
