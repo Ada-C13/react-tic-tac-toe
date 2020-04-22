73f14368f8ef16fc00bb90a2b48a5df9c82c09ea
Antonia <antonianomamiukor@gmail.com>
2020-04-22 04:50:51 -0700

    will attempt to finish winning logic later, commented out for now

--- src/App.js
+++ src/App.js
@@ -56,17 +56,17 @@ const App = () => {
   const checkForWinner = () => {
     // Complete in Wave 3
     const winningCombinations = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 5, 9],
-      [3, 5, 7],
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-    ]
+  //     [1, 2, 3],
+  //     [4, 5, 6],
+  //     [7, 8, 9],
+  //     [1, 5, 9],
+  //     [3, 5, 7],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [3, 6, 9],
+  //   ]
 
-  }
+  // }
 
   const resetGame = () => {
     // Complete in Wave 4
